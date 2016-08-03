e6163e3aec9780cdf6a1d36e6b56aa5a4ebc3776
Fernando Henrique Laranjo Silva <fernandohlaranjo@gmail.com>
2016-08-03 11:25:56 -0300

    Removido target/

--- target/sias-1.0-SNAPSHOT/js/Searcher.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $('#comboBusca').select2(Select2FamiliaConfig);
-    $('#comboBusca').on('change', function (val, choice) {
-        var comboBusca = $("#comboBusca");
-        var familia = comboBusca.val();
-        document.location.assign('/sias/controleFamiliar/familia/' + familia + '/editar');
-    });
-});
