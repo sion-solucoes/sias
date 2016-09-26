062cd74d425afc2e16fb75e19858dce1f29b2a2e
sion-solucoes <sionsolucoes@gmail.com>
2016-09-26 01:07:16 -0300

    Update familiaMembroSaudeController.js

--- src/main/webapp/js/familiaMembroSaudeController.js
+++ src/main/webapp/js/familiaMembroSaudeController.js
@@ -9,7 +9,7 @@ $('#abaSaude').ready(function (event) {
 
     $.ajax({
         method: 'POST',
-        url: '/sias/controleFamiliar/deficiencia/readAll',
+        url: '/controleFamiliar/deficiencia/readAll',
         success: function (data) {
             if (data !== null) {
                 for (var i = 0; i < data.length; i++) {
